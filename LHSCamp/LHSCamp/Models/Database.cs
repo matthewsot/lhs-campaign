4d894d90b1d8f712a11f12fc6387a8f4a7ea1487
matthewsotoudeh <matthewsot@outlook.com>
2014-02-27 16:42:45 -0800

    Cleaned up the folders
    Just can't stand having Scripts/Styles/fonts (seriously, undercase?) as
    basically root folders. At least now it looks prettier :D

---
+++ LHSCamp/LHSCamp/Models/Database.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace LHSCamp.Models
+{
+    // You can add profile data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class User : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class LCDB : IdentityDbContext<User>
+    {
+        public LCDB()
+            : base("LCDB")
+        {
+        }
+    }
+
+    public class ApplicationUserManager : UserManager<User>
+    {
+        public ApplicationUserManager(IUserStore<User> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options)
+        {
+            var manager = new ApplicationUserManager(new UserStore<User>(new LCDB()));
+            // Configure the application user manager
+            manager.UserValidator = new UserValidator<User>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            manager.PasswordValidator = new MinimumLengthValidator(6);
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.PasswordResetTokens = new DataProtectorTokenProvider(dataProtectionProvider.Create("PasswordReset"));
+                manager.UserConfirmationTokens = new DataProtectorTokenProvider(dataProtectionProvider.Create("ConfirmUser"));
+            }
+            return manager;
+        }
+    }
+}
