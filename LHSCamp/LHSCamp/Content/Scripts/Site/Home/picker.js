e4bce398a7fd1df054fd244fb0d6973259e9fee0
matthewsotoudeh <matthewsot@outlook.com>
2014-03-03 19:26:31 -0800

    Listing candidates
    Now listing candidates in a grid. Some bugs, but it's working

---
+++ LHSCamp/LHSCamp/Content/Scripts/Site/Home/picker.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+var pickerBox = (function () {
+    function pickerBox(pickerId) {
+        this.pickerId = pickerId;
+        this.picker = $("#" + pickerId);
+    }
+    pickerBox.prototype.addItem = function (item, row, col) {
+        row = row - 1;
+        col = col - 1;
+        var positionClass = "";
+        switch (col) {
+            case 0:
+                positionClass = "left-item";
+                break;
+            case 1:
+                positionClass = "mid-item";
+                break;
+            case 2:
+                positionClass = "right-item";
+                break;
+        }
+        var newItem = '<div class="picker-item ' + positionClass + '" id="picker-item-' + item.id + '" style="margin-top:' + row * 300 + 'px;">';
+        newItem += '<img class="picker-img" src="' + item.profilePic + '" alt="' + item.name + '"></img>';
+        newItem += '<br/><div class="picker-name">' + item.name + '</div>';
+        newItem += '</div>';
+        this.picker.html(this.picker.html() + newItem);
+    };
+
+    pickerBox.prototype.switchTo = function (position) {
+        var self = this;
+        $.getJSON("/API/Candidates/" + position, function (data) {
+            self.picker.html("");
+            for (var i = 0; i < data.length; i++) {
+                var col = (i + 1) % 3;
+                if (col == 0)
+                    col = 3;
+
+                self.addItem(data[i], Math.ceil((i + 1) / 3), col);
+            }
+        });
+    };
+    return pickerBox;
+})();
+//# sourceMappingURL=picker.js.map
---
+++ LHSCamp/LHSCamp/Content/Scripts/Site/Home/picker.ts
@@ -0,0 +1,45 @@
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+
+class pickerBox {
+    pickerId: string;
+    picker: JQuery;
+    constructor(pickerId) {
+        this.pickerId = pickerId;
+        this.picker = $("#" + pickerId);
+    }
+
+    addItem(item, row: number, col: number) {
+        row = row - 1;
+        col = col - 1;
+        var positionClass = "";
+        switch (col) {
+            case 0:
+                positionClass = "left-item";
+                break;
+            case 1:
+                positionClass = "mid-item";
+                break;
+            case 2:
+                positionClass = "right-item";
+                break;
+        }
+        var newItem = '<div class="picker-item ' + positionClass + '" id="picker-item-' + item.id + '" style="margin-top:' + row * 300 + 'px;">';
+        newItem += '<img class="picker-img" src="' + item.profilePic + '" alt="' + item.name + '"></img>';
+        newItem += '<br/><div class="picker-name">' + item.name + '</div>';
+        newItem += '</div>';
+        this.picker.html(this.picker.html() + newItem);
+    }
+
+    switchTo(position) {
+        var self = this;
+        $.getJSON("/API/Candidates/" + position, function (data) {
+            self.picker.html("");
+            for (var i = 0; i < data.length; i++) {
+                var col = (i + 1) % 3;
+                if (col == 0) col = 3;
+
+                self.addItem(data[i], Math.ceil((i + 1) / 3), col);
+            }
+        });
+    }
+}
