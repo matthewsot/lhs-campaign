480b9d658ca549109bc81645dff0cb27789fb97b
matthewsotoudeh <matthewsot@outlook.com>
2014-03-07 21:18:21 -0800

    So, bundling fonts doesn't really work
    It tried to find the font files in /bundles/, which wasn't gonna happen.
    Just too much work to fix in time, better to leave it like this

--- LHSCamp/LHSCamp/App_Start/BundleConfig.cs
+++ LHSCamp/LHSCamp/App_Start/BundleConfig.cs
@@ -13,19 +13,6 @@ namespace LHSCamp
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Content/Scripts/jQuery/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Content/Scripts/Modernizr/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/Scripts/Bootstrap/bootstrap.js",
-                      "~/Content/Scripts/Bootstrap/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/Styles/Bootstrap/bootstrap.css",
-                      "~/Content/Styles/site.css"));
         }
     }
 }
