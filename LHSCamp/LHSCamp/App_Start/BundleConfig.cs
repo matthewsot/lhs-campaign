64db71615298cc12e62c71e0010c54a26d71e578
matthewsotoudeh <matthewsot@outlook.com>
2014-03-08 17:50:39 -0800

    Faster animations w/ GSAP

--- LHSCamp/LHSCamp/App_Start/BundleConfig.cs
+++ LHSCamp/LHSCamp/App_Start/BundleConfig.cs
@@ -8,8 +8,16 @@ namespace LHSCamp
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery-raw").Include(
                         "~/Content/Scripts/jQuery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Content/Scripts/GSAP/CSSPlugin.min.js",
+                "~/Content/Scripts/GSAP/EasePack.min.js",
+                "~/Content/Scripts/GSAP/TweenLite.min.js",
+                "~/Content/Scripts/jQuery/jquery-{version}.js",
+                "~/Content/Scripts/GSAP/jquery.gsap.min.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Content/Scripts/jQuery/jquery.validate*"));
