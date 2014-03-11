e7b8db40db46ffd8ee0ebba8a1224912888f3fc2
matthewsotoudeh <matthewsot@outlook.com>
2014-03-10 21:46:42 -0700

    Removed unused account views

--- LHSCamp/LHSCamp/Controllers/AccountAPIController.cs
+++ LHSCamp/LHSCamp/Controllers/AccountAPIController.cs
@@ -1,15 +1,9 @@
 ﻿using LHSCamp.Models;
 using Microsoft.AspNet.Identity;
-using Microsoft.Owin.Security;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using Owin;
 
 namespace LHSCamp.Controllers
 {
