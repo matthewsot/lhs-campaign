cf8af5f0997a3f82eabc738746d279abe3816785
matthewsotoudeh <matthewsot@outlook.com>
2015-03-18 21:29:20 -0700

    oh god this codebase...

---
+++ LHSCamp/LHSCamp/Content/Scripts/home.js
@@ -0,0 +1,35 @@
+﻿$("body").on("click", "#position-tabs li", function () {
+    $(this).addClass("selected").siblings(".selected").removeClass("selected");
+    $(".tab-content").hide();
+    $(".tab-content[data-tab=\"" + $(this).text().trim() + "\"]").show();
+});
+$("#position-tabs li").first().click();
+
+$.cookie.json = true;
+function setSelected(val) {
+    $.cookie("selected-attendees", val, { path: "/", expires: 365 });
+    $.cookie("asb-elections", true, { path: "/", expires: 365 });
+}
+function getSelected() {
+    var selected = $.cookie("selected-attendees");
+    if (typeof selected === "undefined") {
+        setSelected([]);
+        return [];
+    }
+    return selected;
+}
+
+function refreshSelected() {
+    
+}
+
+$("body").on("click", ".add-pic-button", function() {
+    var selected = getSelected();
+    var thisId = parseInt($(this).closest(".candidate").attr("data-id"));
+
+    if (selected.indexOf(thisId) === -1) {
+        selected.push(thisId);
+    } else {
+        selected.splice(selected.indexOf(thisId), 1);
+    }
+});
