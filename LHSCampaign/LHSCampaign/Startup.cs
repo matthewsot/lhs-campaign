4a0b0c9ffde4ae87324c60ba08f3e541a90f6269
matthewsotoudeh <matthewsot@outlook.com>
2016-02-25 20:43:43 -0800

    Basic template

---
+++ LHSCampaign/LHSCampaign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LHSCampaign.Startup))]
+namespace LHSCampaign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
