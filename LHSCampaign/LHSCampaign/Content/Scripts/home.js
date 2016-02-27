947eda759973d131e732576e491ee3f399a378d8
matthewsotoudeh <matthewsot@outlook.com>
2016-02-26 18:47:01 -0800

    removed the confetti

--- LHSCampaign/LHSCampaign/Content/Scripts/home.js
+++ LHSCampaign/LHSCampaign/Content/Scripts/home.js
@@ -1,65 +1,16 @@
-﻿var unopposedTimeout = null;
-
-$(".tab-content").hide();
+﻿$(".tab-content").hide();
 
 $("body").on("click", "#position-tabs div", function (e) {
-    if ($("#totally-unopposed").length > 0) {
-        $(".tab-content").hide();
-        $("#totally-unopposed").show();
-        $(this).siblings(".selected").removeClass("selected");
-        return;
-    }
-
     $(this).addClass("selected").siblings(".selected").removeClass("selected");
     $(".tab-content").hide();
 
-    if (unopposedTimeout != null) {
-        clearTimeout(unopposedTimeout);
-    }
-    $("#unopposed-popup").hide();
     var thisFirstText = $(this).clone().children().remove().end().text().trim(); //Thanks! http://viralpatel.net/blogs/jquery-get-text-element-without-child-element/
     if ($(".tab-content[data-tab=\"" + thisFirstText + "\"]").length > 0 && $(".tab-content[data-tab=\"" + thisFirstText + "\"]").children("article").length > 0) {
         $(".tab-content[data-tab=\"" + thisFirstText + "\"]").show();
-
-        var numCandidates = $(".tab-content[data-tab=\"" + thisFirstText + "\"]").children("article").length;
-        var isUnopposed = numCandidates == 1;
-        if (thisFirstText == "vice president") {
-            switch ($("#class-picker .selected").text().trim()) {
-            case "2017":
-                isUnopposed = numCandidates > 0 && numCandidates <= 3;
-                break;
-            case "2018":
-                isUnopposed = numCandidates > 0 && numCandidates <= 2;
-                break;
-            }
-        }
-        if (isUnopposed) {
-            //Celebrate!
-            $('[class*=confetti]').remove();
-            $('html').confetti({
-                x: $(this).offset().left + $(this).width() / 2,
-                y: $(this).offset().top + $(this).height() / 2,
-                complate: function() {
-                }
-            });
-
-            $("#unopposed-popup").css({
-                position: "absolute",
-                left: $(this).offset().left + $(this).width() / 2 - 85,
-                top: $(this).offset().top + $(this).height() / 2 - 35
-            }).fadeIn();
-
-            unopposedTimeout = setTimeout(function() {
-                $("#unopposed-popup").fadeOut();
-            }, 3000);
-        }
     } else {
         $(".tab-content[data-tab=\"no-peeps\"]").show();
     }
 });
-if ($("#totally-unopposed").length > 0) {
-    $("#position-tabs .selected").removeClass("selected");
-}
 $("#position-tabs div").first().click();
 
 $.cookie.json = true;
